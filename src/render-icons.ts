ca387167701dca60ea319c2a740c0cd85041a712
Koen De Keyser <koen.dekeyser@gmail.com>
2018-11-14 22:43:35 +0100

    Adding script to create bindings to Material-UI icons (WIP)

---
+++ src/render-icons.ts
@@ -0,0 +1,31 @@
+import * as Fs from 'fs';
+import * as Path from 'path';
+import * as rimraf from 'rimraf';
+
+const outputDirectory = Path.join(__dirname, '../', 'output-icons');
+
+if (Fs.existsSync(Path.join(outputDirectory, 'reason'))) {
+    rimraf.sync(Path.join(outputDirectory, 'reason'));
+}
+Fs.mkdirSync(Path.join(outputDirectory, 'reason'));
+
+
+const GetIcons = () => {
+    Fs.readdir("./extract/icons", function( err, files) {
+        if (err) {
+	    console.error("Could not find icon sources", err);
+            process.exit(1);
+    }
+    
+    files.forEach( function(file, i) {
+        var extension = Path.extname(file)
+        var filename = Path.basename(file,extension)
+        if (extension == ".js") {
+            var filenameReason = Path.join(outputDirectory,'reason', `${filename}.re`)
+            Fs.writeFileSync(filenameReason,"")
+        }
+    });
+});
+};
+
+GetIcons();
